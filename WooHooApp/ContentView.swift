1ecc1e83aa0a78daad56cf534ccd10ddcd2b94a3
orukahairuka <sakuraierika03301109@gmail.com>
2024-05-16 16:53:38 +0900

    Initial Commit

---
+++ WooHooApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WooHooApp
+//
+//  Created by 櫻井絵理香 on 2024/05/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WooHooApp/WooHooAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WooHooAppApp.swift
+//  WooHooApp
+//
+//  Created by 櫻井絵理香 on 2024/05/16.
+//
+
+import SwiftUI
+
+@main
+struct WooHooAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
