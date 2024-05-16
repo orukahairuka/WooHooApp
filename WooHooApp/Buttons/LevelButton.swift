e0f739990ea266c961103e0d261ed501f8869349
orukahairuka <sakuraierika03301109@gmail.com>
2024-05-16 19:01:41 +0900

    Sound追加

--- WooHooApp/Buttons/LevelButton.swift
+++ WooHooApp/Buttons/LevelButton.swift
@@ -1,64 +1,57 @@
 import SwiftUI
 
 struct LevelButton: View {
+    let buttonCount = 10 // ボタンの数
+    @State var animatingButton: Int? = nil // アニメーション中のボタンのインデックス
 
-  @State var willColorAnimate: Bool = false
-  @State var willScaleAnimate: Bool = false
-  @State var willOffsetAnimate: Bool = false
+    //音楽再生
+    let musicPlayer = SoundPlayer()
 
-  // for reset
-  @State var isResseting: Bool = false
-
-  var body: some View {
-    VStack {
-      Image(systemName: "star.fill")
-        .font(.system(size: 64))
-        .foregroundColor(willColorAnimate ? Color.yellow : Color.gray)
-        .animation(.easeInOut(duration: 0.5), value: willColorAnimate)
-        .scaleEffect(willScaleAnimate ? 1.0 : 0.4)
-        // ★リセット時はアニメーションの方法を変更
-        .animation(
-          isResseting
-          ? .linear(duration: 0.5)
-          : .interpolatingSpring(
-            mass: 1.0,
-            stiffness: 240.0,
-            damping: 12.0,
-            initialVelocity: 20.0
-          ),
-          value: willScaleAnimate
-        )
-        .offset(y: willOffsetAnimate ? 10 : 0)
-        .animation(
-            .timingCurve(0.25, 1.0, 0.5, 1.0, duration: 0.5),
-            value: willOffsetAnimate
-        )
-
-      HStack(spacing: 32) {
-        Button {
-          willScaleAnimate = true
-          willOffsetAnimate = true
-          willColorAnimate = true
-        } label: {
-          Text("スタート")
+    var body: some View {
+        VStack {
+            Grid {
+                ForEach(0..<buttonCount/2, id: \.self) { rowIndex in
+                    GridRow {
+                        ForEach(0..<2) { columnIndex in
+                            let index = rowIndex * 2 + columnIndex
+                            Button {
+                                if animatingButton == index {
+                                    // 同じボタンが連続してタップされた場合は、アニメーションをリセット
+                                    animatingButton = nil
+                                } else {
+                                    // 他のボタンがタップされた場合は、現在のアニメーションをリセットしてから新しいボタンのアニメーションを開始
+                                    animatingButton = index
+                                }
+                                musicPlayer.musicPlayer()
+                            } label: {
+                                Image(systemName: "star.fill")
+                                    .font(.system(size: 64))
+                                    .foregroundColor(animatingButton == index ? Color.yellow : Color.gray)
+                                    .animation(.easeInOut(duration: 0.5), value: animatingButton == index)
+                                    .scaleEffect(animatingButton == index ? 1.0 : 0.4)
+                                    .animation(
+                                        animatingButton == index
+                                        ? .interpolatingSpring(
+                                            mass: 1.0,
+                                            stiffness: 240.0,
+                                            damping: 12.0,
+                                            initialVelocity: 20.0
+                                        )
+                                        : .linear(duration: 0.5),
+                                        value: animatingButton == index
+                                    )
+                                    .offset(y: animatingButton == index ? 10 : 0)
+                                    .animation(
+                                        .timingCurve(0.25, 1.0, 0.5, 1.0, duration: 0.5),
+                                        value: animatingButton == index
+                                    )
+                            }
+                        }
+                    }
+                }
+            }
         }
-        .disabled(willScaleAnimate)
-
-        Button {
-          // ★ リセットボタンタップ時にフラグを更新
-          isResseting = true
-          willScaleAnimate = false
-          willOffsetAnimate = false
-          willColorAnimate = false
-          isResseting = false
-        } label: {
-          Text("リセット")
-        }
-        .disabled(!willScaleAnimate)
-      }
-
     }
-  }
 }
 
 #Preview {
--- WooHooApp/WooHooAppApp.swift
+++ WooHooApp/WooHooAppApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct WooHooAppApp: App {
     var body: some Scene {
         WindowGroup {
-            HomeView()
+            LevelButton()
         }
     }
 }
